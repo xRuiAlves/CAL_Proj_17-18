caf40b7ec9935856e2472483196c34ae223367a5
xRuiAlves <up201606746@fe.up.pt>
2018-03-13 13:21:30 +0000

    Added more tests to ensure Dijkstra quality

--- src/Graph/graphTesting.cpp
+++ src/Graph/graphTesting.cpp
@@ -1,97 +1,78 @@
 #include <iostream>
 #include "Graph.h"
-#include "DNode.h"
 #include "../Algorithms/Dijkstra.h"
-//#include "Algorithms/dijkstra.h"
 
 int main(){
 
-	std::cout << "Up and running...\n" << std::endl;
-
-	Graph<char> g(10);
-
-	int nodeId0 = g.addNode('a');
-	int nodeId1 = g.addNode('b');
-	int nodeId2 = g.addNode('c');
-	int nodeId3 = g.addNode('d');
-	int nodeId4 = g.addNode('e');
-    int nodeId5 = g.addNode('f');
-    int nodeId6 = g.addNode('g');
-    int nodeId7 = g.addNode('h');
+	std::cout << "Starting tests ..." << std::endl;
 
 
-	std::cout << "Node 1 value: " << g.getNodeVal(nodeId1).first << std::endl;
-	std::cout << "Node 2 value: " << g.getNodeVal(nodeId2).first << std::endl;
-	std::cout << "Node 3 value: " << g.getNodeVal(nodeId3).first << std::endl;
-	std::cout << "Node 4 value: " << g.getNodeVal(nodeId4).first << std::endl;
-	std::cout << "Node 5 value: " << g.getNodeVal(nodeId5).first << std::endl << std::endl;
+	// Initialize Graph 1
+	std::cout << "\n\n***************************************";
+	std::cout << "\nTesting Graph 1 ..." << std::endl;
 
-	g.addEdge(nodeId0,nodeId1,314);
-	g.addEdge(nodeId1,nodeId3,216);
-	g.addEdge(nodeId1,nodeId4,1337);
-    g.addEdge(nodeId1, nodeId5, 512);
-    g.addEdge(nodeId1, nodeId2, 16);
-    g.addEdge(nodeId2, nodeId5, 16);
+	Graph<char> graph1(10);
 
-	std::cout << "Edge value between Node 1 and Node 2: " << g.getEdgeVal(nodeId1,nodeId2).first << std::endl;
-	std::cout << "Edge value between Node 2 and Node 4: " << g.getEdgeVal(nodeId2,nodeId4).first << std::endl;
-	std::cout << "Edge value between Node 2 and Node 5: " << g.getEdgeVal(nodeId2,nodeId5).first << std::endl;
-	//std::cout << "Edge value between Node 5 and Node 2: " << g.getEdgeVal(nodeId2,nodeId5).first << std::endl;
+	int nodeId0 = graph1.addNode('a');
+	int nodeId1 = graph1.addNode('b');
+	int nodeId2 = graph1.addNode('c');
+	int nodeId3 = graph1.addNode('d');
+	int nodeId4 = graph1.addNode('e');
+    int nodeId5 = graph1.addNode('f');
+    int nodeId6 = graph1.addNode('g');
+    int nodeId7 = graph1.addNode('h');
 
-	std::cout << "\nGraph Size: " << g.getNumNodes() << std::endl;
-
-
-	// Testing DNode creation
-	DNode dn1(0 , 1 , 100);
-	DNode dn2(1);
-
-	std::cout << "\n" << std::endl;
-
-	std::cout << "Dijkstra Node 1" << std::endl;
-	std::cout << "Node Id: " << dn1.getNodeId() << std::endl;
-	std::cout << "Last Node Id: " << dn1.getLastNodeId() << std::endl;
-	std::cout << "Weight: " << dn1.getTotalWeight() << std::endl;
-
-	std::cout << "\nDijkstra Node 2" << std::endl;
-	std::cout << "Node Id: " << dn2.getNodeId() << std::endl;
-	std::cout << "Last Node Id: " << dn2.getLastNodeId() << std::endl;
-	std::cout << "Weight: " << dn2.getTotalWeight() << std::endl;
-
-	std::cout << "\nApplying Setters...\n" << std::endl;
-
-	dn1.setNodeId(20);
-	dn1.setLastNodeId(15);
-	dn1.setTotalWeight(999.80);
-
-	std::cout << "Dijkstra Node 1" << std::endl;
-	std::cout << "Node Id: " << dn1.getNodeId() << std::endl;
-	std::cout << "Last Node Id: " << dn1.getLastNodeId() << std::endl;
-	std::cout << "Weight: " << dn1.getTotalWeight() << std::endl;
-
-    Dijkstra<char> dijkstra = Dijkstra<char>(g);
-    dijkstra.calcOptimalPath(0,5);
-    dijkstra.printSolution();
-/*
-    try {
-        auto n2Connections = g.getConnections(6);
-
-        for (int i = 0; i < n2Connections.size(); ++i) {
-            std::cout << "Node 6 is connected with node " << n2Connections.at(i).first->id << " via an edge with value " << n2Connections.at(i).second << std::endl;
-        }
-    } catch(Graph<char,int>::NodeNotFound & e) {
-        std::cout << "Could not find any node with id: " << e.id << std::endl;
-    }
+	graph1.addEdge(nodeId0,nodeId1,314);
+	graph1.addEdge(nodeId1,nodeId3,216);
+	graph1.addEdge(nodeId1,nodeId4,1337);
+	graph1.addEdge(nodeId1, nodeId5, 512);
+	graph1.addEdge(nodeId1, nodeId2, 16);
+	graph1.addEdge(nodeId2, nodeId5, 16);
 
 
-    //Testing Dijkstra's Algorithm
-    std::cout << "Testing Dijkstra's Algorithm" << std::endl;
-    auto path = getShortestPath(g, g.getNodeByIndex(nodeId1), g.getNodeByIndex(nodeId6));
+    Dijkstra<char> dijkstra1(graph1);
+	dijkstra1.calcOptimalPath(0,5);
+	dijkstra1.printSolution();
 
-    for (auto node : path) {
-        std::cout << "--" << node.data << std::endl;
-    }*/
 
-	std::cout << "\nProgram ran successfully.\n" << std::endl;
+
+
+
+	// Initialize Graph 2 (Empty Graph)
+	std::cout << "\n\n***************************************";
+	std::cout << "\nTesting Graph 2 (Empty Graph) ..." << std::endl;
+	Graph<unsigned int> graph2(0);
+
+	Dijkstra<unsigned int> dijkstra2(graph2);
+	dijkstra2.calcOptimalPath(0,5);
+	dijkstra2.printSolution();
+
+
+
+
+
+	// Initialize Graph 3 (No solution)
+	std::cout << "\n\n***************************************";
+	std::cout << "\nTesting Graph 3 (Impossible Solution Graph) ..." << std::endl;
+	Graph<bool> graph3(5);
+
+	nodeId0 = graph3.addNode(true);
+	nodeId1 = graph3.addNode(true);
+	nodeId2 = graph3.addNode(false);
+	nodeId3 = graph3.addNode(true);
+
+	graph3.addEdge(nodeId0,nodeId1,13);
+	graph3.addEdge(nodeId0,nodeId2,15);
+	graph3.addEdge(nodeId2,nodeId0,20);
+	graph3.addEdge(nodeId3,nodeId2,71.75);
+	graph3.addEdge(nodeId3,nodeId0,71.75);
+
+	Dijkstra<bool> dijkstra3(graph3);
+	dijkstra3.calcOptimalPath(nodeId0,nodeId3);
+	dijkstra3.printSolution();
+
+
+	std::cout << "\nProgram ran successfully." << std::endl;
 
 	return 0;
 }
