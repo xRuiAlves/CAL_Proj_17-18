61ccadec68f8daf0764f61a0c09075c9d00a7f00
xRuiAlves <up201606746@fe.up.pt>
2018-03-13 14:43:45 +0000

    Created Exceptions file | Move 'defs.h' to Utilities folder

---
+++ src/Utilities/exceptions.h
@@ -0,0 +1,32 @@
+//
+// Created by rui on 13-03-2018.
+//
+
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include "defs.h"
+
+class Exception {
+public:
+    Exception() {}
+};
+
+
+class InvalidNodeId : public Exception {
+public:
+    InvalidNodeId(){}
+};
+
+class NodeNotFound : public Exception {
+public:
+    NodeNotFound(u_int id) : id(id) {}
+
+    u_int id;
+};
+
+
+
+
+
+#endif //EXCEPTIONS_H
